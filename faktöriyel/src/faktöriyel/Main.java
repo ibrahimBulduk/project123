a33c98fcd549fce96af6a4a13c80093bad652f72
ibrahimBulduk <113643156+ibrahimBulduk@users.noreply.github.com>
2022-10-01 19:37:07 +0300

    Add files via upload

---
+++ faktöriyel/src/faktöriyel/Main.java
@@ -0,0 +1,15 @@
+package faktöriyel;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int kaçFaktöriyel = 5 ;
+		int sayaç = 1 ;
+		for (int i = 1; i <= kaçFaktöriyel ; i++) {
+			sayaç = sayaç * i ;
+			
+		}System.out.println(sayaç);
+
+	}
+
+}
