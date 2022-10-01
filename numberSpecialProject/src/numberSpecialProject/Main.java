76ed3704c1b981723236609c5bf37f1324a5dc0a
ibrahimBulduk <113643156+ibrahimBulduk@users.noreply.github.com>
2022-10-01 19:36:53 +0300

    Add files via upload

---
+++ numberSpecialProject/src/numberSpecialProject/Main.java
@@ -0,0 +1,18 @@
+package numberSpecialProject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int n =68 ;
+		int sonuc = 1 ;
+		for (int i = 68; sonuc < i ; ) {
+			sonuc = sonuc * 2 ;
+			System.out.println(sonuc);			
+		}System.out.println("-------------------------------------------");
+		while (sonuc<n) {
+			sonuc = sonuc * 2 ;
+			System.out.println(sonuc);
+		}
+	}
+
+}
